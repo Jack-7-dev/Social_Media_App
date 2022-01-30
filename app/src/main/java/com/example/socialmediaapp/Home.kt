5653aa1ad21bd4bb46bb5a6892c02820a39ff694
Jack-7-dev <jackbower835@gmail.com>
2022-01-30 16:41:35 +0000

    Added feed, profile and settings fragments with hard-coded text not pulled from the database. New apk for the new app, with navigation bar working TODO take images from database, SETTINGS page

--- app/src/main/java/com/example/socialmediaapp/Home.kt
+++ app/src/main/java/com/example/socialmediaapp/Home.kt
@@ -2,12 +2,19 @@ package com.example.socialmediaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
